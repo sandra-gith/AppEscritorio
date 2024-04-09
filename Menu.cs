37dd0dd61099bb058cbf05068303b3283405f63c
sandra-gith <136108536+sandra-gith@users.noreply.github.com>
2024-04-09 10:31:38 -0500

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEscritorio
+{
+    public partial class Menu : Form
+    {
+        Calculo ventanaCalculo;
+
+        public Menu()
+
+        {
+            InitializeComponent();
+            ventanaCalculo = new Calculo();
+        }
+
+        private void btnInciar_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            ventanaCalculo.referenciarVentaMenu(this);
+
+            string nombreTextBox = ingresarDato.Text;
+            ventanaCalculo.atraparDato(nombreTextBox);
+
+            ventanaCalculo.Visible = true;
+
+        }
+
+    }
+}
